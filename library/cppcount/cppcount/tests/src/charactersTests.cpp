9e7ddcc7ce89d41c7cd534086b8e64c21ac54d76
Marc-Antoine Maheux <Marc-Antoine.Maheux@USherbrooke.ca>
2022-03-16 10:53:09 -0400

    Format all C++ files.

--- library/cppcount/cppcount/tests/src/charactersTests.cpp
+++ library/cppcount/cppcount/tests/src/charactersTests.cpp
@@ -8,14 +8,7 @@ TEST(charactersTests, countCharacters)
 {
     unordered_map<char, size_t> counts = countCharacters("Bonjour Bob");
     unordered_map<char, size_t> expected_counts(
-            {{'B', 2},
-             {'o', 3},
-             {'n', 1},
-             {'j', 1},
-             {'u', 1},
-             {'r', 1},
-             {' ', 1},
-             {'b', 1}});
+        {{'B', 2}, {'o', 3}, {'n', 1}, {'j', 1}, {'u', 1}, {'r', 1}, {' ', 1}, {'b', 1}});
 
     EXPECT_TRUE(counts == expected_counts);
 }
@@ -23,14 +16,7 @@ TEST(charactersTests, countCharacters)
 TEST(charactersTests, countCharactersIgnoreCase)
 {
     unordered_map<char, size_t> counts = countCharactersIgnoreCase("BonjOur BOb");
-    unordered_map<char, size_t> expected_counts(
-            {{'B', 3},
-             {'O', 3},
-             {'N', 1},
-             {'J', 1},
-             {'U', 1},
-             {'R', 1},
-             {' ', 1}});
+    unordered_map<char, size_t> expected_counts({{'B', 3}, {'O', 3}, {'N', 1}, {'J', 1}, {'U', 1}, {'R', 1}, {' ', 1}});
 
     EXPECT_TRUE(counts == expected_counts);
 }
