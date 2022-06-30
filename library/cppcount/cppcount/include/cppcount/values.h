32e7075fe53ada043d3d39e8727cfbcaa3d14ca8
mamaheux <35638081+mamaheux@users.noreply.github.com>
2022-06-30 14:09:26 -0400

    Update library/cppcount/cppcount/include/cppcount/values.h
    Co-authored-by: Dominic Létourneau <dominic.letourneau@usherbrooke.ca>

--- library/cppcount/cppcount/include/cppcount/values.h
+++ library/cppcount/cppcount/include/cppcount/values.h
@@ -24,7 +24,7 @@ std::unordered_map<map_result_of_t<T, M>, size_t> countValues(T first, T last, M
 template <class T>
 std::unordered_map<typename std::iterator_traits<T>::value_type, size_t> countValues(T first, T last)
 {
-    return countValues(first, last, [](const typename std::iterator_traits<T>::value_type& x) { return x; });
+    return countValues(first, last, [](const auto& x) { return x; });
 }
 
 #endif
